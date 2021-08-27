c577f29db9f858c86387eb6d2b0419990f7e1d6d
ConsuL <consul.developer@gmail.com>
2021-08-28 02:22:06 +0300

    css

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="app">
-      <header>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <header>
         <router-link :to="{name: 'employees'}">Emploees list</router-link>
       </header>
     <main>
